8b4ceeab371b76e3b9b203d076e5a2cc42ffb1d2
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-02-10 17:17:20 +0100

    margin

--- js/init.js
+++ js/init.js
@@ -7,5 +7,12 @@
       	closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+    Materialize.fadeInImage('#objectifs');
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
