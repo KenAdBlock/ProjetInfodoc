2f5fcaa1b8fef67584ce4893f6de1ea6adae64c1
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-02-11 12:05:28 +0100

    description stage2

--- js/init.js
+++ js/init.js
@@ -14,5 +14,11 @@
 
     Materialize.fadeInImage('#objectifs');
     
+    $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
