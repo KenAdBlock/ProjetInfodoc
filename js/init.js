0a6840262e2099208a1adc9195302b1feb15bee9
GONZALEZ GALLEGOS Edwin <g15023670@A-F001925-L.salsa.univ-amu.fr>
2016-02-12 16:39:31 +0100

    maquette

--- js/init.js
+++ js/init.js
@@ -9,8 +9,15 @@
 
     $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+    $('.modal-trigger').leanModal(
+    {
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .4, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+    });
+
+});
 
     Materialize.fadeInImage('#objectifs');
     
@@ -18,6 +25,10 @@
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
+  });
+
+    $(document).ready(function() {
+    $('select').material_select();
   });
     
   }); // end of document ready
