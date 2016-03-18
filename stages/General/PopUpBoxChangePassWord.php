c42174a291ea2ebb123dd82711b03477c77e3ad0
Amiria Assoumani <amiria.assoumani@gmail.com>
2016-03-18 11:58:02 +0100

    formulaire changePassword

--- stages/General/PopUpBoxChangePassWord.php
+++ stages/General/PopUpBoxChangePassWord.php
@@ -18,7 +18,7 @@
     OpenSession  ($NomSession);
 
 	?>
-    <script>if (! window.opener) location.replace ("<?=$PATH_RACINE?>index.php")</script>
+<script xmlns="http://www.w3.org/1999/html">if (! window.opener) location.replace ("<?=$PATH_RACINE?>index.php")</script>
     <?php
 
     // Vérification qu'il existe bien une session ouverte
@@ -41,8 +41,8 @@
     foreach ($_GET  as $clef => $valeur) $$clef = htmlspecialchars ($valeur);
     foreach ($_POST as $clef => $valeur) $$clef = htmlspecialchars ($valeur);
 	
-	$WidthDfltPopUpBoxChangePW  = 550;
-	$HeightDfltPopUpBoxChangePW = 530;
+	$WidthDfltPopUpBoxChangePW  = 640;
+	$HeightDfltPopUpBoxChangePW = 670;
 	
 	$WidthPopUpBoxChangePW  = $WidthDfltPopUpBoxChangePW + 10;
 	$HeightPopUpBoxChangePW = $HeightDfltPopUpBoxChangePW;
@@ -105,98 +105,77 @@
                    '<?=$HeightPopUpBoxChangePW?>');
 // -->
 </script>
+
 <div class="container">
-      <div class="row">
-        <div class="col s12">
-          <div class="card grey lighten-4 z-depth-1">
-            <div class="card-content">
-            	<span class="card-title"><h5 class="center">Changement de mot de passe</h5></span> 
-<form method="post"> 
-<table align="center" border="0" cellpadding="2" cellspacing="0"> 
-    <tr>
-	    <td colspan="4">
-		    <br /><b>Attention </b>: 
-		    <ul><li>le mot de passe doit comporter entre <b><?=MINLGPASSWD?></b> 
-		    et <b><?=MAXLGPASSWD?></b> chiffres ou lettres (majuscules ou 
-		    minuscules sauf le <b>ç</b>, et sans accent),
+	<div class="row">
+		<div class="col s12">
+			<div class="card grey lighten-4 z-depth-1">
+				<div class="card-content">
+					<span class="card-title"><h5 class="center">Changement de mot de passe :</h5></span>
+						<form method="POST">
+								<p>
+									<b>Attention :</b>
+								</p>
 
-            </li><li>
-		    les <b>majuscules et les minuscules</b> sont considérées comme des
-		     <b>caractères différents</b>.
+								<ul><li>le mot de passe doit comporter entre <b><?=MINLGPASSWD?></b>
+										et <b><?=MAXLGPASSWD?></b> chiffres ou lettres (majuscules ou
+										minuscules sauf le <b>ç</b>, et sans accent),
+									</li><li>
+										les <b>majuscules et les minuscules</b> sont considérées comme des
+										<b>caractères différents</b>.
+									</li></ul>
 
-            </li></ul>
-		</td>
-	</tr>
-	<tr>
-	    <td nowrap valign="top"><?=$Indic ['NewPW1']?></td> 
-	    <td align="right"><nobr><b><?=$LibPW1?></b> <sup>*</sup></nobr></td>
-		<td colspan="4"><div class="input-field col s12"><input type="password" name="NewPW1" id="NewPW1">
-		</div></td>
+								<div class="row">
+									<div class="input-field col s12">
+										<input id="NewPW1" type="password" name="NewPW1" >
+										<label for="NewPW1"><?=$Indic ['NewPW1']?>&nbsp;<nobr><?=$LibPW1?><sup>*</sup></nobr></label>
+									</div>
+									<div class="input-field col s12">
+										<input id="NewPW2" type="password" name="NewPW2" >
+										<label for="NewPW2"><?=$Indic ['NewPW2']?>&nbsp;<nobr><?=$LibPW2?><sup>*</sup></nobr></label>
+									</div>
+								</div>
+								<div class="center">
+									<button type="submit" class="waves-effect waves-light btn blue white-text">Valider</button>
+								</div>
 
-	</tr>
-	<tr>
-	    <td nowrap valign="top"><?=$Indic ['NewPW2']?></td> 
-	    <td align="right"><nobr><b><?=$LibPW2?></b> <sup>*</sup></nobr></td>
-		<td colspan="4"><div class="input-field col s12"><input type="password" name="NewPW2">
-		</div></td>
+							<hr>
+							<blockquote><sup>*</sup>
+							<small>Choisissez un mot de passe composé de
+								<b><?=MINLGPASSWD?></b> à
+								<b><?=MAXLGPASSWD?></b> lettres <br />&nbsp;
+								(majuscules ou minuscules <b>sans accents</b>) ou chiffres</small></blockquote>
+							<hr><br>
+							<?php
+							if (count ($CodErr))
+							{
+							?>
 
-		
-	</tr>
-	<tr><td>&nbsp;</td><td>&nbsp;</td>
-	<td colspan="4" align="right"><button class="btn waves-effect waves-light white-text blue" type="submit">Valider</button></td>
-<td>&nbsp;</td></tr>
-	
-	<tr>
-        <td colspan="4" align="left"
-		    ><blockquote><sup>*</sup>
-			 <small>Choisissez un mot de passe composé de
-			 <b><?=MINLGPASSWD?></b> à
-			 <b><?=MAXLGPASSWD?></b> lettres <br />&nbsp; (majuscules ou
-			 minuscules <b>sans accents</b>) ou chiffres</small></blockquote>
-             <hr>
-		</td>
-    </tr>
-                                                                           <?php
-                                        if (count ($CodErr))
-										{
-                                                                           ?>
-    <tr>
-        <td colspan="4">
-                                                                           <?php										
-                                            while ($Code = array_pop ($CodErr))
-											{
-											    print ('$Code = '.$Code.'<br />');
-                                                if ($Code == $ERRCHAMPNONREMPLI)
-                                                    PrintMsgErr (MsgErrNonInit (array_pop ($NomChampVide)));
-                                                else
-                                                    PrintMsgErr ($TextMsgErr [$Code]);
-											}
-                                                                           ?>
-        </td>
-    </tr>
-	<tr>
-        <td colspan="4" align="center"><hr>
-		</td>
-	</tr>
-                                                                           <?php
-                                        }
-                                                                           ?>
-	<tr>
-        <td class="center" colspan="4" align="center">
-		    <button class="btn waves-effect waves-light white-text blue" type="submit" 
-		           
-		           onClick="window.close()">Fermer la fenetre</button></td>
-    </tr>
-	<tr>
-        <td colspan="4" align="center"><hr></td>
-    </tr>
-</table>
-<input type="hidden" name="StepChPwd" value="Valid">
-</form>
+							<?php
+							while ($Code = array_pop ($CodErr))
+							{
+								print ('$Code = '.$Code.'<br />');
+								if ($Code == $ERRCHAMPNONREMPLI)
+									PrintMsgErr (MsgErrNonInit (array_pop ($NomChampVide)));
+								else
+									PrintMsgErr ($TextMsgErr [$Code]);
+							}
+							?>
+								<?php
+							}
+							?>
+							<p class="center">
+								<button type="submit" class="waves-effect waves-light btn black white-text"
+									onClick="window.close()">Fermer la fenetre</button>
+							</p>
+							<input type="hidden" name="StepChPwd" value="Valid">
+						</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-          </div>
-        </div>
-      </div>
+
   <!--  Scripts-->
       <script src="<?=$URL_SITE.$PATH_JQUERY?>jquery-2.2.1.min.js"></script>
       <script src="<?=$URL_SITE.$PATH_MATERIALIZE?>materialize.min.js"></script>
